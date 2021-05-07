57fab69d22c73909a4859063d4b649aebfce6178
Rodrigo Oliveira <rodrigo.cardoso.oliveira@gmail.com>
2021-05-07 10:48:25 -0300

    eslint

--- pages/api/forma_pagamento/[id].ts
+++ pages/api/forma_pagamento/[id].ts
@@ -1,20 +1,27 @@
-import type { NextApiRequest, NextApiResponse } from 'next'
-import prisma from '../../../lib/prisma'
+import type { NextApiRequest, NextApiResponse } from "next";
+import prisma from "../../../lib/prisma";
 
-export default async function handle(req: NextApiRequest, res: NextApiResponse) {
-    const id = req.query.id
-    const {data} = req.body
-    if(req.method === 'PUT') {
-        console.log(id);
-        console.log(data)
-        const resultPosts = await prisma.formaPagamento.update(  {where: {id: Number(id)},data:{...data}})
-        res.send(resultPosts)
-    }else if (req.method === 'GET') {
-        const resultPosts = await prisma.formaPagamento.findUnique(  {where: {id: Number(id)}})
-        res.send(resultPosts)
-    }else if(req.method === 'DELETE'){
-        const resultPosts = await prisma.formaPagamento.delete(  {where: {id: Number(id)}})
-        res.send(resultPosts)
-    }
-
+export default async function handle(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const { id } = req.query;
+  const { data } = req.body;
+  if (req.method === "PUT") {
+    const resultPosts = await prisma.formaPagamento.update({
+      where: { id: Number(id) },
+      data: { ...data },
+    });
+    res.send(resultPosts);
+  } else if (req.method === "GET") {
+    const resultPosts = await prisma.formaPagamento.findUnique({
+      where: { id: Number(id) },
+    });
+    res.send(resultPosts);
+  } else if (req.method === "DELETE") {
+    const resultPosts = await prisma.formaPagamento.delete({
+      where: { id: Number(id) },
+    });
+    res.send(resultPosts);
+  }
 }
