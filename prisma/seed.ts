37e53af151961982e6bc623ce41d6d4860c95727
Rodrigo Oliveira <rodrigo.cardoso.oliveira@gmail.com>
2021-05-07 11:05:20 -0300

    eslint

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,27 +1,25 @@
-import { PrismaClient, Prisma } from '@prisma/client'
-
-const prisma = new PrismaClient()
-
-const userData: Prisma.UserCreateInput[] = [
-
-]
-
-async function main() {
-  console.log(`Start seeding ...`)
-  for (const u of userData) {
-    const user = await prisma.CartaoCredito.create({
-      data: u,
-    })
-    console.log(`Created user with id: ${user.id}`)
-  }
-  console.log(`Seeding finished.`)
-}
-
-main()
-  .catch((e) => {
-    console.error(e)
-    process.exit(1)
-  })
-  .finally(async () => {
-    await prisma.$disconnect()
-  })
+// import { PrismaClient, Prisma } from "@prisma/client";
+//
+// const prisma = new PrismaClient();
+//
+// const userData: Prisma.UserCreateInput[] = [];
+//
+// async function main() {
+//   console.log(`Start seeding ...`);
+//   for (const u of userData) {
+//     const user = await prisma.CartaoCredito.create({
+//       data: u,
+//     });
+//     console.log(`Created user with id: ${user.id}`);
+//   }
+//   console.log(`Seeding finished.`);
+// }
+//
+// main()
+//   .catch((e) => {
+//     console.error(e);
+//     process.exit(1);
+//   })
+//   .finally(async () => {
+//     await prisma.$disconnect();
+//   });
