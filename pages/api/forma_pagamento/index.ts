dca24574c994c310920ab3051241bac52f7a45f3
Rodrigo Oliveira <rodrigo.cardoso.oliveira@gmail.com>
2021-05-07 11:04:16 -0300

    eslint

--- pages/api/forma_pagamento/index.ts
+++ pages/api/forma_pagamento/index.ts
@@ -1,18 +1,22 @@
-import type { NextApiRequest, NextApiResponse } from 'next'
-import prisma from '../../../lib/prisma'
+import type { NextApiRequest, NextApiResponse } from "next";
+import prisma from "../../../lib/prisma";
 
-export default async function handle(req: NextApiRequest, res: NextApiResponse) {
-
-    if(req.method === 'GET') {
-        const resultPosts = await prisma.formaPagamento.findMany()
-        res.send(resultPosts)
-    } else if(req.method === 'POST') {
-        const {data} = req.body
-        const resultPosts = await prisma.formaPagamento.create({data:{...data}})
-        res.send(resultPosts)
-    }else {
-        throw new Error(
-            `The HTTP ${req.method} method is not supported at this route.`
-        )
-    }
+export default async function handle(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method === "GET") {
+    const resultPosts = await prisma.formaPagamento.findMany();
+    res.send(resultPosts);
+  } else if (req.method === "POST") {
+    const { data } = req.body;
+    const resultPosts = await prisma.formaPagamento.create({
+      data: { ...data },
+    });
+    res.send(resultPosts);
+  } else {
+    throw new Error(
+      `The HTTP ${req.method} method is not supported at this route.`
+    );
+  }
 }
