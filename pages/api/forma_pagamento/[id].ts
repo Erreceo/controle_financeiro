b72a3e3e0ce5914ab7320bd39bc6efc3ba9f07e7
Rodrigo Oliveira <rodrigo.cardoso.oliveira@gmail.com>
2021-05-07 09:31:04 -0300

    init

---
+++ pages/api/forma_pagamento/[id].ts
@@ -0,0 +1,20 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import prisma from '../../../lib/prisma'
+
+export default async function handle(req: NextApiRequest, res: NextApiResponse) {
+    const id = req.query.id
+    const {data} = req.body
+    if(req.method === 'PUT') {
+        console.log(id);
+        console.log(data)
+        const resultPosts = await prisma.formaPagamento.update(  {where: {id: Number(id)},data:{...data}})
+        res.send(resultPosts)
+    }else if (req.method === 'GET') {
+        const resultPosts = await prisma.formaPagamento.findUnique(  {where: {id: Number(id)}})
+        res.send(resultPosts)
+    }else if(req.method === 'DELETE'){
+        const resultPosts = await prisma.formaPagamento.delete(  {where: {id: Number(id)}})
+        res.send(resultPosts)
+    }
+
+}
