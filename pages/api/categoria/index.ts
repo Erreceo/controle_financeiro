7678ae3847b975ed44fd257dc3bac249cd735c93
Rodrigo Oliveira <rodrigo.cardoso.oliveira@gmail.com>
2021-05-10 14:31:58 -0300

    .

--- pages/api/categoria/index.ts
+++ pages/api/categoria/index.ts
@@ -8,5 +8,13 @@ export default async function handle(
   if (req.method === "GET") {
     const result = await prisma.categoria.findMany();
     res.send(result);
+  } else if (req.method === "POST") {
+    const { data } = req.body;
+    const resultPost = await prisma.categoria.create({ data: { ...data } });
+    res.send(resultPost);
+  } else {
+    throw new Error(
+      `The HTTP ${req.method} method is not supported at this route`
+    );
   }
 }
